91d8d3cbf967e4c811ecde48aea63d11bc400811
Service Web <sw@gssmail.com>
2024-03-14 12:28:24 -0500

    Commit changes in C:\Websites\Git\emarlow\5617\GCG_5617_QTEDIRMNT.sql

---
+++ GCG_5617_QTEDIRMNT.sql
@@ -0,0 +1,11 @@
+-- Database: GLOBALPLA Metadata version: 2 Exported: Aug 8, 2019
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_5617_QTEDIRMNT" USING 'GCG_5617_QTEDIRMNT.mkd' PAGESIZE=4096 (
+ "BaseDir" CHAR(300),
+ "TemplateDir" CHAR(300),
+ "ReplacementString" CHAR(50) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
